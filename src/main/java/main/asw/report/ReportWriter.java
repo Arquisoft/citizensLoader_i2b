541c49853bb1f411a6fc5f8bcc35a42b979a3d54
Jorge López <nokutu@openmailbox.org>
2017-02-14 12:27:28 +0100

    Added parser and report interfaces

---
+++ src/main/java/main/asw/report/ReportWriter.java
@@ -0,0 +1,13 @@
+package main.asw.report;
+
+/**
+ * Created by jorge on 14/02/2017.
+ */
+public interface ReportWriter {
+
+    /**
+     * Writes the report into the file system.
+     * TODO: add parameters
+     */
+    void writeReport();
+}
