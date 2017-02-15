46d32e92e60f761b907f75f941febce69eabc87c
DESKTOP-V22K81K\Pineirin <pineirin@gmail.com>
2017-02-15 10:30:07 +0100

    Factoria creada.

--- src/main/java/main/asw/report/Factory.java
+++ src/main/java/main/asw/report/Factory.java
@@ -3,5 +3,21 @@ package main.asw.report;
 /**
  * Created by Pineirin on 14/02/2017.
  */
-public interface Factory {
+public class Factory {
+    private ReportWriter reportWriter;
+
+    public ReportWriter createTxtWriter(){
+        reportWriter = new TxtWriter();
+        return reportWriter;
+    }
+
+    public ReportWriter createDocxWriter(){
+        reportWriter = new DocxWriter();
+        return reportWriter;
+    }
+
+    public ReportWriter createPdfWriter(){
+        reportWriter = new PdfWriter();
+        return reportWriter;
+    }
 }
