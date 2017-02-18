0054ce31d2346dcb8d60466cf480f8d7d8aa6717
Pineirin <pineirin@gmail.com>
2017-02-18 21:57:18 +0100

    Commit fixed.

--- src/test/java/main/asw/ReportWriterTest.java
+++ src/test/java/main/asw/ReportWriterTest.java
@@ -2,6 +2,7 @@ package main.asw;
 
 import main.asw.report.Factory;
 import main.asw.report.ReportWriter;
+import main.asw.user.User;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,23 +13,26 @@ import static org.junit.Assert.assertEquals;
 public class ReportWriterTest {
     @Test
     public void testTxtWriter() {
+        User[] users = new User[0];
         Factory factory = new Factory();
         ReportWriter reportWriter = factory.createTxtWriter();
-        reportWriter.writeReport("Primero.txt", "Adios");
+        reportWriter.writeReport("Primero.txt", users );
     }
 
     @Test
     public void testDocxWriter() {
+        User[] users = new User[0];
         Factory factory = new Factory();
         ReportWriter reportWriter = factory.createDocxWriter();
-        reportWriter.writeReport("Segundo.docx", "Adios");
+        reportWriter.writeReport("Segundo.docx", users);
     }
 
     @Test
     public void testPdfWriter() {
+        User[] users = new User[0];
         Factory factory = new Factory();
         ReportWriter reportWriter = factory.createPdfWriter();
-        reportWriter.writeReport("Tercero.pdf", "Adios");
+        reportWriter.writeReport("Tercero.pdf", users);
     }
 
 
